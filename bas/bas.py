6fcc9c709dce12415e16c8f7c90941077f4020a1
edu-mateescoj <114930481+edu-mateescoj@users.noreply.github.com>
2025-04-16 15:19:01 +0200

    idées dans bas.py

--- bas/bas.py
+++ bas/bas.py
@@ -22,19 +22,53 @@ def ast_to_mermaid(code):
     return "flowchart TD\\n"+"\\n".join(graph)
 
 code = '''x = 10
+y = 12
 if x > 5:
     print("grand")
 else:
     print("petit")'''
-print(ast_to_mermaid(code).replace("\\n", "\n"))
+#print(ast_to_mermaid(code).replace("\\n", "\n"))
 
-'''
+class mynode(object):
+    def __init__(self, node, node_from = None, node_to = None):
+        self.node_type = type(node).__name__
+        
+    def __str__(self):
+        if isinstance(node, ast.Assign):
+            return "["+ node.targets[0].id + "=" + node.value.n + "]"
+        elif isinstance(node, ast.If):
+            return "{" + node.targets[0].id + "}"
+        
+
+    def set_to(self, id_to):
+        self.node_to = id_to
+    def set_from(self, id_from):
+        self.node_from = id_from
+
 tree = ast.parse(code)
-for node in ast.iter_child_nodes(tree):
-    print(node)
-    if isinstance(node, ast.Assign):
-        print(type(node).__name__)
-'''
+#print(ast.dump(tree, indent=2))  # Affichage + compact
+
+
+for child in ast.iter_child_nodes(tree):
+    print("-----------------------------------")
+    if isinstance(child, ast.Assign):
+        #print(type(child).__name__)
+        #print(child.targets[0].id, "=", child.value.n)  # Affichage de la cible de l'assignation
+        print((str((astor.to_source(child)))).strip())
+        #print(str(id(child)))
+    elif isinstance(child, ast.If):
+        #print(type(child).__name__)
+        print((str(astor.to_source(child))).strip().splitlines())
+        #print("Condition:", ast.dump(child.test))
+        #print("Corps: A CONSTRUIRE AVEC...", ast.dump(child))
+    elif isinstance(child, ast.Expr):
+        print(type(child).__name__)
+        print("Expression:", ast.dump(child.value))
+    else:
+        print("\n AUTRE : ",type(child).__name__)
+
+    print('line',child.lineno, 'col',child.col_offset)  # pour info
+
 
 ##### SOURCE
 '''https://docs.python.org/3/library/ast.html
